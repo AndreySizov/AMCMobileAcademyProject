107cb8305b110bc224406bad8a4a39e5a3b8806b
AndreySizov <ansiz777@yandex.ru>
2020-03-02 18:28:11 +0300

    Initial Commit

---
+++ AMCMobileAcademyProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AMCMobileAcademyProject
+//
+//  Created by Андрей on 02/03/2020.
+//  Copyright © 2020 AMC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
